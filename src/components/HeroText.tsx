39b2e389f61a42a3c337adcc66d7a71f80b27ae7
Neeraj Patil <110386298+Neeraaaj@users.noreply.github.com>
2024-01-15 10:09:10 +0530

    Update HeroText.tsx

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -9,14 +9,15 @@ const HeroText = () => {
         initial={{height: 0}}
         animate={{height: "unset"}}
         transition={{delay: animationStart, duration: 0.5}}
-    className='flex flex-col items-center text-center mt-[-5vw] sm:mt-[-5vw]'>
+    className='flex flex-col items-center text-center'>
         <motion.div 
             variants={reveal}
             initial="hiddenVariant"
             animate="revealedVariant"
             transition={{delay: animationStart + 1, duration: 0.6}}
-        className='flex flex-col text-3vw <md:text-24px font-bold mb-30px pt-100px'>
-            <span className='flex items-center gap-10px'>Delivering <SVG /> Industry's</span>
+        className='flex flex-col text-2xl lg:text-6xl sm:text-3xl <md:text-1xl font-bold mb-30px pt-100px '>
+            <span className='flex 
+            text-4xl sm:text-4xl lg:text-8xl items-center gap-10px'>Delivering <SVG /> Industry's</span>
             <span>Top Talents Work</span>
         </motion.div>
         <motion.div 
@@ -24,7 +25,9 @@ const HeroText = () => {
             initial="hiddenVariant"
             animate="revealedVariant"
             id='sub-head'
-            transition={{delay: animationStart + 1.2, duration: 0.6}} className='mb-30px w-[50vw] <lg:w-1/3 <md:w-1/2 text-2.5vw leading-tight text-gray-700'>
+            transition={{delay: animationStart + 1.2, duration: 0.6}} className='mb-30px w-[100%]
+            sm:w-[100%] <lg:w-[100%] <md:w-[100%] text-1xl 
+            lg:text-4xl leading-tight text-gray-700'>
             Revolutionizing the Digital Landscape: Elevate Your Experience with Our Freelance Maestros in Crafting Cutting-Edge Web Apps and Mobile Solutions
         </motion.div>
     </motion.div>

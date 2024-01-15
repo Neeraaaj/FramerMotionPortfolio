e6787d9dcd055d00849d06bab3d8d3cffd44c1bf
Neeraj Patil <110386298+Neeraaaj@users.noreply.github.com>
2024-01-15 15:18:46 +0530

    Update BoatVideo.tsx

--- src/components/BoatVideo.tsx
+++ src/components/BoatVideo.tsx
@@ -1,10 +1,9 @@
-import WebSrc from '../assets/web-design2.mp4'
 
 const BoatVideo = () => {
   return (
     <video className='h-100vh w-full object-cover animate-clip-from-top-animation
     shadow-lg' autoPlay muted loop>
-        <source src={WebSrc} type="video/mp4" />
+        <source src={"https://cdn.dribbble.com/userupload/3738676/file/original-80c2e8faa083b3fbf34bacc067ad8614.mp4"} type="video/mp4" />
     </video>
   )
 }

03d3e1309a24ef1968c77cb2f9c5ddae66543fc6
Neeraj Patil <110386298+Neeraaaj@users.noreply.github.com>
2024-04-17 20:13:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import './App.css'
 import About from './components/About'
 import ProjectGrid from './components/ProjectGrid'
 import Contact from './components/Contact'
-import Video from './components/Video'
 
 function App() {
 
